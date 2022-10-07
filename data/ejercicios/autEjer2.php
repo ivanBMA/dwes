2dc4ff7fe5fc1225705d021663d38ca629d8bc1e
ivanCasa <ivanCasa@>
2022-10-07 10:55:15 +0100

    casa 07/10/2022

--- data/ejercicios/autEjer2.php
+++ data/ejercicios/autEjer2.php
@@ -1,3 +1,27 @@
+<?php
+/*https://rafacabeza.gitbook.io/dws/php*/
+    $url = "http://ejercicios/ejercicio2.html";
+
+    function redirecciona(){
+        //redirect($url);
+        //yaf_controller_abstract::redirect($url)
+        header('Location://ejercicios/ejercicio2.html');
+    }
+
+    
+    if(isset($_POST["nombre"]) && !empty($_POST["nombre"])){
+        $titulo = $_POST["nombre"];
+        if($titulo == "juan" || $titulo == "pedro"){
+            echo "<br> te saludo : " . $titulo;
+        }
+        
+        else{
+            redirecciona();
+        }
+    }
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,23 +30,7 @@
         <title>Document</title>
 </head>
     <body>
-        
-        <?php
-        /*https://rafacabeza.gitbook.io/dws/php*/
-
-            if(isset($_POST["nombre"]) && !empty($_POST["nombre"])){
-                $titulo = $_POST["nombre"];
-                if($titulo == "juan" || $titulo == "pedro"){}
-                echo "<br> te saludo : " . $titulo;
-            }
-            else{
-                echo "<br> <h3 style=color:#FF0000>EL titulo no se a introducido </h3>" ;
-                header('Location: [/ejercicio2.html')];
-                exit;
-            }
-
-            
-        ?>
+       
 
         <form name="miformu" action="/ejercicio2.html" method="post">
             <input type="submit" name="envio" value="return">
